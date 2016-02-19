ca5c17438b521d2448e902c7b9fb7fda4ac45575
Grzegorz Kokoszka <grzegorz.r.kokoszka@gmail.com>
2016-02-19 16:23:16 +0100

    Piłka
    odbijanie piłki

--- src/pong/Ball.java
+++ src/pong/Ball.java
@@ -1,18 +1,88 @@
 package pong;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Ball {
     
-    public int x, y, width , height;
+    public int x, y, width =25 , height = 25, motionX, motionY, ammountOfHits;
     
-    public Ball(){
-        
+    public Random random;
+    
+    private Pong pong;
+    
+    public Ball(Pong pong){
+        this.pong = pong;
+        this.random = new Random();
+        spawn();
     }
     public void update(Paddle p1, Paddle p2){
+        int speed = 5;
+        this.x += motionX * speed;
+        this.y += motionY * speed;
         
+        if(this.y + height > pong.height || this.y < 0){
+                if(this.motionY <0){
+                    this.motionY = random.nextInt(4);
+                }
+                else{
+                    this.motionY = -random.nextInt(4);
+                }
+            }
+        
+        if(checkCollision(p1) == 1){
+            this.motionX = 1 + (ammountOfHits / 5);
+            this.motionY = -2 + random.nextInt(4);
+            if(motionY == 0){
+                motionY =1;
+            }
+            ammountOfHits++;
+        }else if(checkCollision(p2) == 1){
+            this.motionX = -1 - (ammountOfHits / 5);
+            this.motionY = -2 + random.nextInt(4);
+            if(motionY == 0){
+                motionY =1;
+            }
+            ammountOfHits++;
+        }if(checkCollision(p1) == 2){
+            p2.score++;
+            spawn();
+        }
+        else if(checkCollision(p2) == 2){
+            p1.score++;
+            spawn();
+        }
     }
-    public void render(Graphics g){
+    
+    public void spawn(){
+        ammountOfHits = 0;
+        this.x = pong.width / 2 - this.width /2;
+        this.y = pong.height /2 - this.height /2;
+        motionY = -2 + random.nextInt(4);
+        if(motionY == 0){
+            motionY =1;
+        }
+        if(random.nextBoolean()){
+            motionX = 1;
+        }else {
+            motionX = -1;
+        }
+    }
+    
+    public int checkCollision(Paddle paddle){
         
+            if(this.x < paddle.x + paddle.width && this.x + width > paddle.x && this.y < paddle.y + paddle.height && this.y + height > paddle.y){
+                return 1; // hit
+                
+            } else if((paddle.x > x && paddle.paddleNumber == 1) || (paddle.x < x && paddle.paddleNumber == 2)) {
+                return 2; // score
+            }
+        return 0; //nothing
+    }
+    
+    public void render(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillOval(x, y, width, height);
     }
 }
--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -1,8 +1,10 @@
 package pong;
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -13,8 +15,10 @@ public class Pong implements ActionListener, KeyListener{
 
     public static Pong pong;
     public Paddle p1, p2;
+    public Ball ball;
     public int width = 700, height = 700;
     public boolean bot = false, w, s, up, down;
+    public int gameStatus = 0; //0 = Stopped, 1= Stop, 2=Playing
     
     public Renderer renderer;
     
@@ -29,13 +33,14 @@ public class Pong implements ActionListener, KeyListener{
         frame.add(renderer);
         frame.addKeyListener(this);
         
-        start();
         
         timer.start();
     }
     public void start(){
+        gameStatus = 2;
         p1 = new Paddle(this, 1);
         p2 = new Paddle(this, 2);
+        ball = new Ball(this);
     }
     public void update(){   
         if(w == true){
@@ -50,18 +55,40 @@ public class Pong implements ActionListener, KeyListener{
         if(down == true){
             p2.move(false);
         }
+        ball.update(p1, p2);
     }
     public void render(Graphics2D g){
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, width, height);
-        g.setColor(Color.GRAY);
-        g.setStroke(new BasicStroke(5f));
-        g.drawLine(width/2, 0, width/2, height);
-        p1.render(g);
-        p2.render(g);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if(gameStatus == 0){
+            g.setColor(Color.GREEN);
+            g.setFont(new Font("Arial",1 ,30));
+            g.drawString("Pong", width/2 -35, 50);
+            g.setColor(Color.RED);
+            g.drawString("Press Space to start game", width/2 -190, 300);
+            g.drawString("Press Shift to play with a bot", width/2 -190, 350);
+        }
+        if(gameStatus == 2 || gameStatus == 1){
+            g.setColor(Color.GRAY);
+            g.setStroke(new BasicStroke(5f));
+            g.drawLine(width/2, 0, width/2, height);
+            p1.render(g);
+            p2.render(g);
+            ball.render(g);
+        }
+        if(gameStatus == 1){
+            g.setColor(Color.GREEN);
+            g.setFont(new Font("Arial",1 ,30));
+            g.drawString("Pause", width/2 -35, 50);
+        }
+        
     }
     public void actionPerformed(ActionEvent e) {
-        update();
+        if(gameStatus == 2){
+           update(); 
+        }
+        
         renderer.repaint();
     }
     public static void main(String[] args) {
@@ -86,6 +113,23 @@ public class Pong implements ActionListener, KeyListener{
         if(id == KeyEvent.VK_DOWN){
             down = true;
         }
+        if(id == KeyEvent.VK_SHIFT && gameStatus == 0){
+            bot = true;
+            start();
+        }
+        if(id == KeyEvent.VK_SPACE){
+            if(gameStatus == 0){
+                start();
+                bot = false;
+            }
+            else if(gameStatus == 1){
+                gameStatus = 2;
+            }
+            else if(gameStatus == 2){
+                gameStatus = 1;
+            }
+            
+        }
     }
 
     public void keyReleased(KeyEvent e) {
@@ -102,5 +146,6 @@ public class Pong implements ActionListener, KeyListener{
         if(id == KeyEvent.VK_DOWN){
             down = false;
         }
+        
     }
 }
